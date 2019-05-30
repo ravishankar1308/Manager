a652d58250da62cae08267ffb46682c47fcbb3ec
ravi shankar <ravishankar.0813@gmail.com>
2019-05-30 18:56:09 +0530

    120 compare

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,10 @@
-import { EMAIL_CHANGED } from './types';
+import firebase from 'firebase';
+import { 
+  EMAIL_CHANGED, 
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+ } from './types';
+
 
 export const emailChanged = (text) => {
   return {
@@ -6,3 +12,20 @@ export const emailChanged = (text) => {
     payload: text
   };
 };
+
+export const passwordChanged = (text) =>{
+  return{
+    type: PASSWORD_CHANGED,
+    payload:text
+  }
+}
+
+export const loginUser = ({email, password}) => {
+  return(dispatch) => {
+firebase.auth().signInWithEmailAndPassword(email, password)
+.then( user =>{
+  dispatch({type : 'LOGIN_USER_SUCCESS', payload: user});
+});
+};
+};
+ 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
@@ -8,6 +8,14 @@ class LoginForm extends Component {
     this.props.emailChanged(text);
   }
 
+  onPasswordChange(text){
+    this.props.passwordChanged(text);
+  }
+
+  onButtonPress(){
+    const {email, password} = this.props;
+    this.props.loginUser({ email, password});
+  }
   render() {
     return (
       <Card>
@@ -25,11 +33,13 @@ class LoginForm extends Component {
             secureTextEntry
             label="Password"
             placeholder="password"
+            onChangeText={this.onPasswordChange.bind(this)}
+            value={this.props.password}
           />
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Login
           </Button>
         </CardSection>
@@ -40,8 +50,11 @@ class LoginForm extends Component {
 
 const mapStateToProps = state => {
   return {
-    email: state.auth.email
+    email: state.auth.email,
+    password: state.auth.password
   };
 };
 
-export default connect(mapStateToProps, { emailChanged })(LoginForm);
+export default connect(mapStateToProps, { 
+  emailChanged , passwordChanged, loginUser
+})(LoginForm);
